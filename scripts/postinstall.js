d2e2a5abfca979242ef83945b3c3805419ceaa89
gianfebrian <gianfebrian@gmail.com>
2016-02-09 11:56:18 +0700

    remove models dir creation

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,6 @@
 
 var fs = require('fs');
 var path = require('path');
-var mkdirp = require('mkdirp');
 
 var srcFile   = path.normalize(__dirname + '/../config/mongoose.js');
 var destFile = path.normalize(process.cwd() + '/../../config/mongoose.js');
@@ -11,5 +10,3 @@ if(!fs.existsSync(destFile)) {
   console.log("Copying " + srcFile + " to " + destFile);
   fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
 }
-
-mkdirp.sync(path.normalize(process.cwd() + '/../../models'));
