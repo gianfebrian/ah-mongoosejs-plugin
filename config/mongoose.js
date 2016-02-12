3c41dcb49c576350611c9c3e667bffdea93b33a6
gianfebrian <gianfebrian@gmail.com>
2016-02-12 22:50:44 +0700

    change config to default

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ exports.default = {
   mongoose: function(api) {
     return {
       auto_start: true,
-      connection_string: "mongodb://touring:touring@127.0.0.1:27017/touring_default",
+      connection_string: "mongodb://USER:PASSWORD@HOST:PORT/DATABASE",
       debug: true,
       model_path: api.project_root + '/document_models'
     }
@@ -13,7 +13,7 @@ exports.test = {
   mongoose: function(api) {
     return {
       auto_start: false,
-      connection_string: "mongodb://touring:touring@127.0.0.1:27017/touring_test",
+      connection_string: "mongodb://USER:PASSWORD@HOST:PORT/DATABASE",
       debug: false,
       model_path: api.project_root + '/document_models'
     }
